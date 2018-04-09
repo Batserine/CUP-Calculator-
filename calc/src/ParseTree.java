1a6325d3c228989fd2f23ccd41024f225f561645
Balakrishna <balu636216@gmail.com>
2018-04-09 19:04:16 +0700

    Add files via upload

---
+++ calc/src/ParseTree.java
@@ -0,0 +1,12 @@
+/*
+ This defines how the nodes (operaators, terminals and non-terminals) are associated with parse tree 
+ */
+package src;
+
+import java.util.ArrayList;
+
+public class ParseTree {
+    private ArrayList<ParseTree> nodes = new ArrayList<>();
+    private String value;
+    
+}
